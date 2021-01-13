e0a222540a69477777d40c0050835f167a0ca071
millakortelainen <milla.kortelainen@gmail.com>
2021-01-13 14:30:24 +0000

    Update main to output generated at 92c29c8

---
+++ build/static/js/main.1e701d6b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfrontend=this.webpackJsonpfrontend||[]).push([[0],{9:function(e,n,t){"use strict";t.r(n);var r=t(1),o=(t(0),t(3)),c=function(){return Object(r.jsx)("div",{children:"hello2!"})};t.n(o).a.render(Object(r.jsx)(c,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.1e701d6b.chunk.js.map
